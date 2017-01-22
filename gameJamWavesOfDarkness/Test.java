dc228ebfd4ece7cd1e48405ad5797e0ff2a3b401
Billy Everyteen <guygizmo@gmail.com>
2017-01-21 21:26:08 -0500

    Fixed issue with combining paths in windows

--- gameJamWavesOfDarkness/Test.java
+++ gameJamWavesOfDarkness/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import tiled.core.Map;
 import tiled.core.MapLayer;
 import tiled.core.ObjectGroup;
@@ -5,9 +6,16 @@ import tiled.core.TileLayer;
 import tiled.io.TMXMapReader;
 
 class Test2 {
+    public static String combine(String path1, String path2)
+    {
+        File file1 = new File(path1);
+        File file2 = new File(file1, path2);
+        return file2.getPath();
+    }
+
     public void printSomething(String dataPath) {
         try {
-            Map map = new TMXMapReader().readMap(dataPath + "/sewers.tmx");
+            Map map = new TMXMapReader().readMap(combine(dataPath, "sewers.tmx"));
             System.out.print("Level width: " + map.getWidth() + "\n");
             System.out.print("Level height: " + map.getHeight() + "\n");
         } catch (Exception e) {
